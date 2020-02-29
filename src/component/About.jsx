d5aef51d9a61edb55cd344c05816be28a4f10c25
jbeans05 <blueice.exploit@gmail.com>
2020-02-29 18:02:05 +0700

    Update About.jsx
    Change Content

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
 const About =()=>{
-    return <span>Hallo saya dodo</span>
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div style="posotion:absolut; top: 100px;">
+                    <div className="col-md-6 offset-2">
+                        <p>
+                            <h2>About Me</h2>
+                            <p>
+                                Hallo my name is dodi rustandi <br/>
+                                now working in ReactJs Application Developer
+                            </p>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
 }
 
 export default About;
