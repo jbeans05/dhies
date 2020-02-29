58cb1eca93c3819efc0268f72d9e7d04aabeb9ea
jbeans05 <blueice.exploit@gmail.com>
2020-02-29 18:11:19 +0700

    Update About.jsx
    change worng tag

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -3,23 +3,18 @@ import React from 'react'
 const About =()=>{
     return (
         <div className="container-fluid">
-            <div className="row">
-                <div style="posotion:absolut; top: 100px;">
-                    <div className="col-md-6 offset-2">
+            <div className="row">             
+                <div className="col-md-6 offset-2">
+                    <p>
+                        <h2>About Me</h2>
                         <p>
-                            <h2>About Me</h2>
-                            <p>
-                                Hallo my name is dodi rustandi <br/>
-                                now working in ReactJs Application Developer
-                            </p>
+                            Hallo my name is dodi rustandi <br/>
+                            now working in ReactJs Application Developer
                         </p>
-
-                    </div>
-                </div>
+                    </p>
+                </div>               
             </div>
         </div>
-
-
     )
 }
 
